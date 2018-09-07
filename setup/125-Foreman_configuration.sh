4e943f1443f5095dce6325d563c50f3abc1b023a
Andre Ramoni <andre.ramoni@gmail.com>
2018-09-07 00:18:24 -0300

    .

--- setup/125-Foreman_configuration.sh
+++ setup/125-Foreman_configuration.sh
@@ -2,117 +2,166 @@
 #Hammer:
 #https://www.lisenet.com/2018/katello-create-products-repositories-content-views-lifecycle-environments-activation-keys/
 
-hammer domain update --name "ext.home.lab" --organizations "RLabs" --locations "Home"
+. vars || ( echo "vars file not found" ; exit 1 )
 
+#############################
+echo -n "* [domain update: ${EXT_DOMAIN}] "
+hammer domain update \
+  --name "${EXT_DOMAIN}" \
+  --organizations "${ORGANIZATION}" --locations "${LOCATION}"
+
+echo -n "* [subnet create: ${EXT_NETWORK_NAME} - ${EXT_NETWORK_ADDRESS}] "
 hammer subnet create \
-  --name "ext.lab" \
-  --network "192.168.12.0" \
-  --mask "255.255.255.0" \
-  --gateway "192.168.12.1" \
+  --name "${EXT_NETWORK_NAME}" \
+  --network "${EXT_NETWORK_ADDRESS}" \
+  --mask "${EXT_NETWORK_NETMASK}" \
+  --gateway "${EXT_NETWORK_GATEWAY}" \
   --boot-mode "Static" \
-  --from "192.168.12.100" \
-  --to "192.168.12.149" \
-  --domains "ext.home.lab" \
-  --dns-primary "192.168.12.2" \
+  --from "${EXT_NETWORK_RANGE_FROM}" \
+  --to "${EXT_NETWORK_RANGE_TO}" \
+  --domains "${EXT_DOMAIN}" \
+  --dns-primary "${EXT_DNS1}" \
   --ipam "Internal DB" \
-  --organizations "RLabs" \
-  --locations "Home"
+  --organizations "${ORGANIZATION}" \
+  --locations "${LOCATION}"
 
-hammer domain create --name "prov.home.lab" --organizations "RLabs" --locations "Home"
+#############################
+echo -n "* [domain create: ${PROV_DOMAIN}] "
+hammer domain create --name "${PROV_DOMAIN}" --organizations "${ORGANIZATION}" --locations "${LOCATION}"
 
+echo -n "* [subnet create: ${PROV_NETWORK_NAME} - ${PROV_NETWORK_ADDRESS}] "
 hammer subnet create \
-  --name "prov.home.lab" \
-  --network "10.255.0.0" \
-  --mask "255.255.0.0" \
-  --gateway "10.255.0.1" \
+  --name "${PROV_NETWORK_NAME}" \
+  --network "${PROV_NETWORK_ADDRESS}" \
+  --mask "${PROV_NETWORK_NETMASK}" \
+  --gateway "${PROV_NETWORK_GATEWAY}" \
   --boot-mode "DHCP" \
-  --from "10.255.10.1" \
-  --to "10.255.100.254" \
-  --domains "prov.lab" \
+  --from "${PROV_NETWORK_RANGE_FROM}" \
+  --to "${PROV_NETWORK_RANGE_TO}" \
+  --domains "${PROV_DOMAIN}" \
   --ipam "DHCP" \
-  --organizations "RLabs" \
-  --locations Home
+  --organizations "${ORGANIZATION}" \
+  --locations "${LOCATION} 
+
+#############################
+echo -n "* [sync-plan create: Daily] "
+hammer sync-plan create --interval 'daily' --name 'Daily' --enabled 'yes'  --organization "${ORGANIZATION}" --sync-date "`date -I`" 
 
 
-hammer sync-plan create --interval 'daily' --name 'Daily' --enabled 'yes'  --organization 'RLabs' --sync-date "`date -I`" 
+#############################
+echo -n "* [product create: CentOS7] "
+hammer product create --name "CentOS7" --organization "${ORGANIZATION}" --sync-plan 'Daily'
 
-
-hammer product create --name "CentOS7" --organization "RLabs" --sync-plan 'Daily'
-
+echo -n "* [repository create: CentOS7|CentOS7 base] "
 hammer repository create --product="CentOS7" \
   --content-type="yum" --name "CentOS7 base" \
   --url "http://mirror.centos.org/centos/7/os/x86_64" \
-  --download-policy "on_demand" --organization "RLabs" 
+  --download-policy "on_demand" --organization "${ORGANIZATION}" 
 
-
-
+echo -n "* [repository create: CentOS7|CentOS7 extras] "
 hammer repository create --product="CentOS7" \
   --content-type="yum" --name "CentOS7 extras" \
   --url "http://mirror.centos.org/centos/7/extras/x86_64" \
-  --download-policy "on_demand" --organization "RLabs"
+  --download-policy "on_demand" --organization "${ORGANIZATION}"
 
-
+echo -n "* [repository create: CentOS7|CentOS7 updates] "
 hammer repository create --product="CentOS7" \
   --content-type="yum" --name "CentOS7 updates" \
   --url "http://mirror.centos.org/centos/7/updates/x86_64" \
-  --download-policy "on_demand" --organization "RLabs"
+  --download-policy "on_demand" --organization "${ORGANIZATION}"
 
-
-
+echo -n "* [repository create: CentOS7|Epel 7] "
 hammer repository create --product="CentOS7" \
   --content-type="yum" --name "Epel 7" \
   --url "http://download.fedoraproject.org/pub/epel/7/x86_64" \
-  --download-policy "on_demand" --organization "RLabs"
+  --download-policy "on_demand" --organization "${ORGANIZATION}"
 
-
-
-
+echo -n "* [repository create: CentOS7|Katello Client ${KATELLO_VERSION}] "
 hammer repository create --product="CentOS7" \
-  --content-type="yum" --name "Katello Client 3.8" \
-  --url "https://fedorapeople.org/groups/katello/releases/yum/3.8/client/el7/x86_64" \
-  --download-policy "on_demand" --organization "RLabs"
+  --content-type="yum" --name "Katello Client ${KATELLO_VERSION}" \
+  --url "https://fedorapeople.org/groups/katello/releases/yum/${KATELLO_VERSION}/client/el7/x86_64" \
+  --download-policy "on_demand" --organization "${ORGANIZATION}"
 
-
-
+echo -n "* [repository create: CentOS7|Puppet 4 PC1] "
 hammer repository create --product="CentOS7" \
   --content-type="yum" --name "Puppet 4 PC1" \
   --url "http://yum.puppetlabs.com/el/7/PC1/x86_64" \
-  --download-policy "on_demand" --organization "RLabs"
+  --download-policy "on_demand" --organization "${KATELLO_VERSION}"
+
+#############################
+echo -n "* [repository synchronize: CentOS7 base] "
+hammer repository synchronize --name "CentOS7 base" --product "CentOS7" --organization "${ORGANIZATION}" --async
+
+echo -n "* [repository synchronize: CentOS7 extras] "
+hammer repository synchronize --name "CentOS7 extras" --product "CentOS7" --organization "${ORGANIZATION}" --async
+
+echo -n "* [repository synchronize: CentOS7 updates] "
+hammer repository synchronize --name "CentOS7 updates" --product "CentOS7" --organization "${ORGANIZATION}" --async
+
+echo -n "* [repository synchronize: Katello Client ${KATELLO_VERSION}] "
+hammer repository synchronize --name "Katello Client ${KATELLO_VERSION}" --product "CentOS7" --organization "${ORGANIZATION}" --async
+
+echo -n "* [repository synchronize: Epel 7] "
+hammer repository synchronize --name "Epel 7" --product "CentOS7" --organization "${ORGANIZATION}" --async
+
+echo -n "* [repository synchronize: Puppet 4 PC1] "
+hammer repository synchronize --name "Puppet 4 PC1" --product "CentOS7" --organization "${ORGANIZATION}" --async
+
+#############################
+echo -n "* [content-view create: CentOS7] "
+hammer content-view create --name "CentOS7" --organization "${ORGANIZATION}"  --product "CentOS7" --repositories "CentOS7 base,CentOS7 extras,CentOS7 updates,Epel 7,Katello Client ${KATELLO_VERSION},Puppet 4 PC1"
+
+echo -n "* [content-view publish: CentOS7] "
+hammer content-view publish --name "CentOS7" --organization "${ORGANIZATION}"
+
+#############################
+echo -n "* [activation-key create: CentOS7] "
+hammer activation-key create --content-view "CentOS7" --name "CentOS7" --organization "${ORGANIZATION}"
+
+#############################
+echo -n "* [medium create: CentOS7 local] "
+hammer medium create --name "CentOS local" \
+  --organization "${ORGANIZATION}" \
+  --os-family "Redhat" \
+  --path "http://${FOREMAN_HOSTNAME}/pulp/repos/${ORGANIZATION}/Library/custom/CentOS7/CentOS7_base/" \
+  --locations "${LOCATION}"
+
+#############################
+echo -n "* [os update: ID 1 media CentOS local] "
+hammer os update --id 1 --media "CentOS local"
 
 
-
-
-
-hammer repository synchronize --name "CentOS7 base" --product "CentOS7" --organization "RLabs"
-
-hammer repository synchronize --name "CentOS7 extras" --product "CentOS7" --organization "RLabs"
-hammer repository synchronize --name "CentOS7 updates" --product "CentOS7" --organization "RLabs"
-hammer repository synchronize --name "Katello Client 3.8" --product "CentOS7" --organization "RLabs"
-hammer repository synchronize --name "Epel 7" --product "CentOS7" --organization "RLabs"
-hammer repository synchronize --name "Puppet 4 PC1" --product "CentOS7" --organization "RLabs"
-
-
-
-
-hammer content-view create --name "CentOS7" --organization "RLabs"  --product "CentOS7" --product "CentOS7" --repositories "CentOS7 base,CentOS7 extras,CentOS7 updates,Epel 7,Katello Client 3.8,Puppet 4 PC1"
-
-
-hammer content-view publish --name "CentOS7" --organization "RLabs"
-
-
-hammer activation-key create --content-view "CentOS7" --name "CentOS7" --organization "RLabs"
-
-hammer medium create --organization "RLabs" --os-family "Redhat" --name "CentOS local" --path "http://foreman.ext.home.lab/pulp/repos/RLabs/Library/custom/CentOS/CentOS7_base/"
-
-
-
+#############################
+echo -n "* [global-parameter set: disable-firewall=true] "
 hammer global-parameter set --name "disable-firewall" --value "true"
+
+echo -n "* [global-parameter set: enable-puppetlabs-pc1-repo=true] "
 hammer global-parameter set --name "enable-puppetlabs-pc1-repo" --value "true"
+
+echo -n "* [global-parameter set: kt_activation_keys=CentOS7] "
 hammer global-parameter set --name "kt_activation_keys" --value "CentOS7"
+
+echo -n "* [global-parameter set: selinux-mode=disabled] "
 hammer global-parameter set --name "selinux-mode" --value "disabled"
+
+echo -n "* [global-parameter set: runinteral=600] "
 hammer global-parameter set --name "runinterval" --value "600"
 
+#############################
+echo -n "* [location set-parameter: http-proxy=${PROXY}] "
+hammer location set-parameter --location "Home" --name "http-proxy" --value "${PROXY}"
+echo -n "* [location set-parameter: http-proxy-port=${PROXY_PORT}] "
+hammer location set-parameter --location "Home" --name "http-proxy-port" --value "${PROXY_PORT}"
 
-hammer location set-parameter --location "Home" --name "http-proxy" --value "proxy.ext.home.lab"
-hammer location set-parameter --location "Home" --name "http-proxy-port" --value "3128"
+
+#############################
+echo -n "* [compute-resource create: ${COMPUTE_RESOURCE_NAME}] "
+hammer compute-resource create --name "${COMPUTE_RESOURCE_NAME}" \
+  --organization "${ORGANIZATION}" --locations "${LOCATION}" \
+  --provider "${COMPUTE_RESOURCE_PROVIDER}" \
+  --server "${COMPUTE_RESOURCE_SERVER}" \
+  --user "${COMPUTE_RESOURCE_USER}" \
+  --password "${COMPUTE_RESOURCE_PASSWORD}" \
+  --datacenter "${COMPUTE_RESOURCE_DATACENTER}" 
+
  
